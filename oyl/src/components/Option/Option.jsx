5014c2cdeb82541b31e9605038c22e565d70ca9f
Lucila Werner <lulikwerner@gmail.com>
2023-06-18 15:39:24 -0400

    pregFrecuentas

---
+++ oyl/src/components/Option/Option.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import '../Option/Option.css';
+
+const Item = ({ opt }) => {
+  const { img, title, description, foot } = opt;
+
+  return (
+    <div>
+      <Card className="cardhover">
+        <section>
+          <div className="containerP">
+            <div className="imgColumn">
+              <img src={img} alt="" className="imgCard" />
+            </div>
+            <div className="contentColumn" id="quienes-somos">
+              <h1>{title}</h1>
+              <p className="prueba" dangerouslySetInnerHTML={{ __html: description }}></p>
+              <div className="centered-text" dangerouslySetInnerHTML={{ __html: foot }}></div>
+            </div>
+          </div>
+        </section>
+      </Card>
+    </div>
+  );
+};
+
+export default Item;
---
+++ oyl/src/components/OptionList/OptionList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import Option from '../Option/Option';
+
+
+
+const OptionList = ({ options }) => {
+  return (
+    <div className="">
+      {options.map((opt) => (
+        <Card key={opt.id} className="">
+          <Option opt={opt} />
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default OptionList;
