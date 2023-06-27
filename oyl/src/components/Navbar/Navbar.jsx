d85f9e440b2cf19e55dec7dcc8e0de436fbeda61
Lucila Werner <lulikwerner@gmail.com>
2023-06-26 21:39:33 -0400

    other navbar

--- oyl/src/components/Navbar/Navbar.jsx
+++ oyl/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
-import '../Navbar/Navbar.css';
+import { NavLink } from 'react-router-dom';
 import logo from '../../../public/assets/img/logo.png';
-import { NavLink, Link } from 'react-router-dom';
+import '../Navbar/Navbar.css';
 
 const Navbar = () => {
   return (
     <header>
-      <nav className="container-Nav">
-        <div>
-          <NavLink to="/" className="Option-Nav" activeclassname="active">Inicio</NavLink>
-        </div>
-        <div>
+      <nav>
+        <ul>
+          <li className="logo">
+            <NavLink to="/" className="logo">
+              <img src={logo} alt="OYL logo" />
+            </NavLink>
+          </li>
+          <input type="checkbox" id="check" />
+          <div className="menu">
+            <li className="inicio">
+            <NavLink to="/" className="Option-Nav" activeclassname="active">Inicio</NavLink>
+            </li>
+            <div className="produNav">
           <a href="https://wa.me/+541136806292?text=hola%20lu%20te%20consulto%20por%20este%20producto" className="Option-Nav">Productos</a>
         </div>
-        <div className="centered-logo">
-          <Link to="/">
-            <img className="imagen-Logo" src={logo} alt="logo" />
-          </Link>
-        </div>
-        <div>
-          <NavLink to="/PreguntasFrecuentes" className="Option-Nav" activeclassname="active">Preguntas Frecuentes</NavLink>
-        </div>
+            <li className="pregFrec">
+            <NavLink to="/PreguntasFrecuentes" className="Option-Nav" activeclassname="active">Preguntas Frecuentes</NavLink>
+           </li>
+          
+            <label htmlFor="check" className="close-menu">
+              <i className="fas fa-times"></i>
+            </label>
+          </div>
+          <label htmlFor="check" className="open-menu">
+            <i className="fas fa-bars"></i>
+          </label>
+        </ul>
       </nav>
     </header>
   );
 };
 
 export default Navbar;
+
+
+
 
 
